90bc0b60d1fc920eebb490cd62918b0578168a66
mattbbc <55093072+mattbbc@users.noreply.github.com>
2020-06-03 17:39:04 +0100

    Display the three versions only

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import './App.css';
 import Waveform from './Waveform';
+import PeaksPlayer from './PeaksPlayer';
+import WaveformHooks from './WaveformHooks';
 
 function App() {
   return (
     <div className="App">
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
-      <Waveform src="sample.wav" />
+      {/* Waveform display using Peaks.js */}
+      <PeaksPlayer />
+      {/* Waveform display using Wavesurfer.js */}
+      <Waveform src="07024205.mp3" />
+      <WaveformHooks src="07024205.mp3" />
     </div>
   );
 }
