0370da8f67edbfba194abdba0ebd8aabad3bea6c
mattbbc <55093072+mattbbc@users.noreply.github.com>
2021-02-12 15:07:43 +0000

    Add wav download

--- src/App.js
+++ src/App.js
@@ -11,6 +11,63 @@ function App() {
   var chunks = [];
 
   useEffect(() => {
+    // This is bodged together to get it working, nothing more
+    
+    // https://www.russellgood.com/how-to-convert-audiobuffer-to-audio-file/#render-as-wav
+    function bufferToWave(abuffer, len) {
+      var numOfChan = abuffer.numberOfChannels,
+          length = len * numOfChan * 2 + 44,
+          buffer = new ArrayBuffer(length),
+          view = new DataView(buffer),
+          channels = [], i, sample,
+          offset = 0,
+          pos = 0;
+    
+      // write WAVE header
+      setUint32(0x46464952);                         // "RIFF"
+      setUint32(length - 8);                         // file length - 8
+      setUint32(0x45564157);                         // "WAVE"
+    
+      setUint32(0x20746d66);                         // "fmt " chunk
+      setUint32(16);                                 // length = 16
+      setUint16(1);                                  // PCM (uncompressed)
+      setUint16(numOfChan);
+      setUint32(abuffer.sampleRate);
+      setUint32(abuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
+      setUint16(numOfChan * 2);                      // block-align
+      setUint16(16);                                 // 16-bit (hardcoded in this demo)
+    
+      setUint32(0x61746164);                         // "data" - chunk
+      setUint32(length - pos - 4);                   // chunk length
+    
+      // write interleaved data
+      for(i = 0; i < abuffer.numberOfChannels; i++)
+        channels.push(abuffer.getChannelData(i));
+    
+      while(pos < length) {
+        for(i = 0; i < numOfChan; i++) {             // interleave channels
+          sample = Math.max(-1, Math.min(1, channels[i][offset])); // clamp
+          sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // scale to 16-bit signed int
+          view.setInt16(pos, sample, true);          // write 16-bit sample
+          pos += 2;
+        }
+        offset++                                     // next source sample
+      }
+    
+      // create Blob
+      return new Blob([buffer], {type: "audio/wav"});
+    
+      function setUint16(data) {
+        view.setUint16(pos, data, true);
+        pos += 2;
+      }
+    
+      function setUint32(data) {
+        view.setUint32(pos, data, true);
+        pos += 4;
+      }
+    }
+
     mediaRecorder.ondataavailable = function(event) {
       chunks.push(event.data);
     }
@@ -30,13 +87,19 @@ function App() {
       clipContainer.appendChild(clipLabel);
       document.body.appendChild(clipContainer);
 
-      var blob = new Blob(chunks, { 'type': 'audio/webm;codecs=opus' });
+      var blob = new Blob(chunks);
       chunks = [];
-      var audioURL = window.URL.createObjectURL(blob);
-      audio.src = audioURL;
-      const downloadButton = document.getElementById("downloadbutton");
-      downloadButton.href = audioURL;
-      downloadButton.download = 'mix.webm';
+
+      blob.arrayBuffer().then(arrayBuffer => {
+        ctx.decodeAudioData(arrayBuffer, audioBuffer => {
+          const wav = bufferToWave(audioBuffer, audioBuffer.length);
+          var audioURL = window.URL.createObjectURL(wav);
+          audio.src = audioURL;
+          const downloadButton = document.getElementById("downloadbutton");
+          downloadButton.href = audioURL;
+          downloadButton.download = 'mix.wav';
+        })
+      });
     }
 
     document.getElementById('startbutton').addEventListener('click', function() {
@@ -55,7 +118,7 @@ function App() {
 
       {/* Waveform display using Wavesurfer.js */}
       {/* <Waveform src="07024205.mp3" /> */}
-      
+
       {/* Demonstrating mix recording */}
       <WaveformHooks src="07024205.mp3" ctx={ctx} dest={dest} />
       <WaveformHooks src="stereo-test.mp3" ctx={ctx} dest={dest} />
